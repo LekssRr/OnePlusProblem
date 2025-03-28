b5088a5539db7b3b982121bf81056caaf3c92245
Alex <sasha_knyazev_2016@bk.ru>
2025-03-28 12:54:48 +0300

    One Commit

---
+++ src/main/java/org/example/Hippo.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@Entity
+public class Hippo {
+    @Id
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "hippo", fetch = FetchType.LAZY)
+    private List<Meal> meals;
+
+}
---
+++ src/main/java/org/example/Meal.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Meal {
+    @Id
+    private Long id;
+    private String type;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Hippo hippo;
+
+    // getters and setters
+}
